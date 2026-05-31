984d50c0da62860edbb4e13d710bd98bc6ef1c67
juanjoilla <juanjo239@hotmail.com>
2026-05-30 23:52:11 -0500

    Change base path in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Proyecto_HerramientasDesarrollo/',
+  base: '/HerramientasDesarrollo/',
   test: {
     globals: true,
     environment: 'jsdom',
